f9323b4b0f6cc4721c08bb0aaa58937930f689d2
Gustavo de Sousa Passos <gustavos.passos@gmail.com>
2020-10-11 16:47:37 -0300

    Alterações no exercicio 52

--- python/ex052.py
+++ python/ex052.py
@@ -1,11 +1,16 @@
 cont = int(input('Digite um número: '))
 count = 0
-for c in range(1,cont):
-  if c % cont == 0:
-    print(c)
+for c in range(1,cont+1):
+  if cont % c == 0:
+    print(f'\033[1;32m{c}\033[m', end=" ")
     count += 1
   else:
-    print(c)
+    print(f'\033[1;31m{c}\033[m', end=" ")
 
-print(f'O número {cont} foi divisivel {count} vezes')
+print(f'\nO número {cont} foi divisivel {count} vez(es)')
+
+if count > 1:
+  print('Por isso ele não é primo!')
+else:
+  print('Por isso ele é primo')
 
